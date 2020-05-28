a0e8426f4cc2f3ce3693af2ce4a89aca6219c59e
Santiago Bonessi <santiagobonessi@hotmail.com>
2020-05-29 09:25:12 +1000

    useState and useEffect examples

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+
 
 export default function App() {
   const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const json = await res.json();
+    setUsers(json);
+    setLoading(false);
+  };
+
+  const incrementNumber = async () => setCount(count + 1)
+  
+
+  useEffect( () => {
+    fetchUsers();
+  },[]);
+
   return (
     <View style={styles.container}>
-      <Text>Hello Aussie!</Text>
+      <Text 
+      style={styles.textName}
+      >
+        {loading === true ? 'Loading...' : users[0].name}
+      </Text>
+      <Text 
+      style={styles.textCount}
+      onPress={incrementNumber}
+      >
+        {count} ⬆️
+      </Text>
     </View>
   );
 }
@@ -16,5 +45,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textName: {
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  textCount: {
+    fontSize: 26,
   },
 });
