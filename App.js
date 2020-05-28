e9360bc254dbfe665a91f1286561832958fdd524
Santiago Bonessi <santiagobonessi@hotmail.com>
2020-05-28 10:02:51 +1000

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <View style={styles.container}>
+      <Text>Hello Aussie!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
