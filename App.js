e60c77dbb27ef99888de6fddd347493d87e32452
Santiago Bonessi <santiagobonessi@hotmail.com>
2020-05-29 11:16:43 +1000

    add useMemo example

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import React, {useState, useEffect, useReducer} from 'react';
+import React, {useState, useEffect, useReducer, useMemo} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// useReducer 
 const initialState = {
   count: 0,
 };
-
 const reducer = (state, action) => {
   switch (action.type) {
     case 'increase':{
@@ -19,25 +19,53 @@ const reducer = (state, action) => {
   }
 };
 
+// useMemo
+const population = [
+  {name: 'James', age: 28}, 
+  {name: 'Maria', age: 64},
+  {name: 'Amy', age: 35}, 
+  {name: 'Sam', age: 42},
+  {name: 'Tommas', age: 38},
+  {name: 'Michael', age: 67},
+  {name: 'Jhon', age: 40}
+];
+
 export default function App() {
+  // useState example
   const [count, setCount] = useState(0);
   const [loading, setLoading] = useState(true);
   const [users, setUsers] = useState([]);
+  // useReducer example
   const [state, dispach] = useReducer(reducer, initialState);
-  
+  // useMemo example
+  const totalAge = useMemo(() => {
+    console.log('Calculating total...');
+    let age = 0;
+    population.forEach( x => {
+      age += x.age;
+    });
+
+    return age;
+  }, [population]);
+  const averageAge = useMemo(() => {
+    console.log('Calculating average...');
+    let countPopul = population.length; 
+    return (totalAge/countPopul).toFixed();
+  }, [population]);
+
+  const incrementNumber = async () => setCount(count + 1)
+  // useEffect example
+  useEffect( () => {
+    fetchUsers();
+  },[]);
+
   const fetchUsers = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
     const json = await res.json();
     setUsers(json);
     setLoading(false);
   };
-
-  const incrementNumber = async () => setCount(count + 1)
   
-  useEffect( () => {
-    fetchUsers();
-  },[]);
-
   return (
     <View style={styles.container}>
       <>
@@ -64,6 +92,11 @@ export default function App() {
         onPress={() => dispach({type:'decrease'})}
         > ⬇️9️⃣</Text>
       </View>
+      <View>
+        <Text style={styles.textMemo}>Population {population.map(x => ` Name: ${x.name} age: ${x.age}`)}</Text>
+        <Text style={styles.textMemo}>Total Age: {totalAge}</Text>
+        <Text style={styles.textMemo}>Average Age: {averageAge}</Text>
+      </View>
     </View>
   );
 }
@@ -86,10 +119,14 @@ const styles = StyleSheet.create({
   },
   viewReducer: {
     flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   textCountReducer: {
     fontSize: 38,
   },
+  textMemo: {
+    fontSize: 24,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+    padding: 8,
+  }
 });
